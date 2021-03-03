de3f70dc1f1d5b8b4ac53d50b111a794d19c2cd5
Edissonal <edissonalonso@gmail.com>
2021-03-02 19:50:42 -0500

    carga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChartsModule, ThemeService } from "ng2-charts";
+import { LineaComponent } from './components/linea/linea.component';
+import { LineasComponent } from './components/lineas/lineas.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    LineasComponent,
+    BarraComponent,
+    RadarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [
+    ThemeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/barra/barra.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-barra',
+  templateUrl: './barra.component.html',
+  styleUrls: ['./barra.component.css']
+})
+export class BarraComponent implements OnInit {
+
+  public doughnutChartLabels: Label[] = ['chuis de pechan', 'chuis alonso', 'chuis burro'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public numeros_ramdom() {
+    this.doughnutChartData = [[
+     Math.round( Math.random() * 100),
+     Math.round( Math.random() * 100),
+     Math.round( Math.random() * 100),
+    ]]
+  }
+}
